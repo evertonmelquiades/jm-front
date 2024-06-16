10d36ccb32c911356373a35523d9ba14caf59a92
melquiades.everton <melquiades.everton@gmail.com>
2024-06-16 18:39:42 -0300

    Add cors to routes

--- src/app/pages/user/user.service.ts
+++ src/app/pages/user/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from './user.model';
@@ -7,28 +7,34 @@ import { User } from './user.model';
   providedIn: 'root'
 })
 export class UserService {
+  private headers = new HttpHeaders({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  });
 
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>('https://api-jm.vercel.app/api/api/users');
+    return this.http.get<User[]>('https://api-jm.vercel.app/api/api/users', { headers: this.headers });
   }
 
   getUser(id: number): Observable<User> {
-    return this.http.get<User>(`https://api-jm.vercel.app/api/api/users${id}`);
+    return this.http.get<User>(`https://api-jm.vercel.app/api/api/users${id}`, { headers: this.headers });
   }
 
   updateUser(user: User): Observable<User> {
-    return this.http.put<User>(`https://api-jm.vercel.app/api/api/users${user.id}`, user);
+    return this.http.put<User>(`https://api-jm.vercel.app/api/api/users${user.id}`, user, { headers: this.headers });
   }
 
   createUser(user: User): Observable<User> {
-    return this.http.post<User>('https://api-jm.vercel.app/api/api/users', user);
+    return this.http.post<User>('https://api-jm.vercel.app/api/api/users', user, { headers: this.headers });
   }
 
   deleteUser(user: User): Observable<User> {
-    return this.http.delete<User>(`https://api-jm.vercel.app/api/api/users${user.id}`);
+    return this.http.delete<User>(`https://api-jm.vercel.app/api/api/users${user.id}`, { headers: this.headers });
   }
 }
+
 
 
